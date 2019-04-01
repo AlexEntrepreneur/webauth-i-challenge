7d072faeb29f8e79e7a4bc62f064bb1c2daed144
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-01 15:45:27 +0100

    Implement Endpoint Functionality

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const knex = require('knex');
 const knexConfig = require('./knexfile');
 const PORT = 5000;
 const server = express();
 const db = knex(knexConfig.development);
+const fakeToken = Buffer.from(Math.random() + '').toString('base64');
 
 server.use(express.json());
 
@@ -12,15 +14,75 @@ server.get('/', (req, res) => {
 });
 
 server.post('/api/register', (req, res) => {
-  res.send('hit register endpoint');
+  let { username, password } = req.body;
+  const hash = bcrypt.hashSync(password, 7);
+
+  password = hash;
+
+  db('users').insert({ username, password })
+    .then(value => {
+      res.send('Added User!');
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `failed to create user ${err}`
+      });
+    });
 });
 
 server.post('/api/login', (req, res) => {
-  res.send('hit login endpoint');
+  let user;
+  let { username, password } = req.body;
+
+  db('users').select('*').where('username', username).first()
+    .then(userObj => {
+      if (userObj) {
+        user = userObj;
+        return user;
+      }
+      else {
+        throw '';
+      }
+    })
+    .then(userObj => {
+      const passwordIsCorrect = bcrypt.compareSync(password, userObj.password);
+      if (passwordIsCorrect) {
+        res.json({
+          token: fakeToken
+        });
+      }
+      else {
+        throw '';
+      }
+    })
+    .catch(err => {
+      res.status(401).json({
+        message: `Bad credentials. Try again.`
+      })
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `failed to get users ${err}`
+      });
+    })
 });
 
 server.get('/api/users', (req, res) => {
-  res.send('You shall not pass!');
+  const { authorization } = req.headers;
+  if (authorization === fakeToken) {
+    db('users').select('username')
+    .then(usersArray => {
+      res.json(usersArray);
+    })
+    .catch(err => {
+      res.status(500).json({
+        message: `failed to get users ${err}`
+      });
+    });
+  }
+  else {
+    res.status(401).send('You shall not pass! You must authenticate!');
+  }
 });
 
 server.listen(PORT, () => {
