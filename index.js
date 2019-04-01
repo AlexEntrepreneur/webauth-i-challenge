01eb3b2a46642f9783d0ec209ccd7d1d1b7ddfa8
Alex Entrepreneur <hello@alexentrepreneur.com>
2019-04-01 14:38:41 +0100

    Add Endpoint Placeholders

--- index.js
+++ index.js
@@ -5,6 +5,24 @@ const PORT = 5000;
 const server = express();
 const db = knex(knexConfig.development);
 
+server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send('Welcome To My Auth Test API');
+});
+
+server.post('/api/register', (req, res) => {
+  res.send('hit register endpoint');
+});
+
+server.post('/api/login', (req, res) => {
+  res.send('hit login endpoint');
+});
+
+server.get('/api/users', (req, res) => {
+  res.send('You shall not pass!');
+});
+
 server.listen(PORT, () => {
   console.log(`\nServer listening on port ${PORT}...\n`);
 });
